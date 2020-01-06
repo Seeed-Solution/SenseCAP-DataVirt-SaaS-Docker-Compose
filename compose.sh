b04b4b921bd9f39c45fd4c7a7d3e1f8a08bc19c2
AscGoogle <wanyou.guo@seeed.cc>
2020-01-06 09:21:07 +0800

    update sh

--- compose.sh
+++ compose.sh
@@ -2,12 +2,15 @@
 project_path=$(cd `dirname $0`; pwd)/../
 
 cd ${project_path} 
+
+rm -rf SenseCAP-DataVirt-SaaS-Example/
 #拉取源码
 git clone https://github.com/Seeed-Solution/SenseCAP-DataVirt-SaaS-Example.git 
 #移动修的.env文件到项目路径
-mv .env SenseCAP-DataVirt-SaaS-Example/
 
-cd SenseCAP-DataVirt-SaaS-Example/
+cp SenseCAP-DataVirt-SaaS-Docker-Compose/.env  SenseCAP-DataVirt-SaaS-Example/
+
+cd  SenseCAP-DataVirt-SaaS-Example/
 
 docker-compose build
 docker-compose up

608e00eeae143c31d034ef7afeb6f04bf1eec558
AscGoogle <wanyou.guo@seeed.cc>
2020-01-09 10:55:13 +0800

    update

---
+++ load.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+docker load -i images/webdemo.tar
+docker load -i images/webapi.tar
+docker load -i images/mysql.tar
+
+echo 'load images success'
+echo 'start git clone code'
+
+project_path=$(cd `dirname $0`; pwd)/
+
+cd ${project_path}
+
+rm -rf SenseCAP-DataVirt-SaaS-Example/
+
+git clone https://github.com/Seeed-Solution/SenseCAP-DataVirt-SaaS-Example.git
+#cp .env file to saas
+cp SenseCAP-DataVirt-SaaS-Example/sensecap_demo/static/lang   lang/
+cp .env  SenseCAP-DataVirt-SaaS-Example/
+cp images/docker-compose.yaml SenseCAP-DataVirt-SaaS-Example/
+
+cd  SenseCAP-DataVirt-SaaS-Example/
+echo 'start build '
+docker-compose build
+docker-compose up -d
+
+echo -e "Server is running, please visit \033[43;37mhttp://localhost:6060"

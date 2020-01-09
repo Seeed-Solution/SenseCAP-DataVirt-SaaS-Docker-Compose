0f54c626f1f63602a70dccfefc340a43121c07c9
jancee <jancee.wang@qq.com>
2020-01-09 14:34:03 +0800

    add windows-helper-load.bat

---
+++ windows-helper-load.bat
@@ -0,0 +1,2 @@
+docker load -i images/seeed-docker-ubuntu-1.0.tar
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v %cd%:/saasc jancee/seeed-docker-ubuntu:1.0 sh -c "cd /saasc && chmod +x compose.sh && dos2unix ./compose.sh && dos2unix ./replace-logo.sh && dos2unix ./replace-title.sh && dos2unix ./remove-all.sh && dos2unix ./load.sh && ./compose.sh; cd /saasc && bash"

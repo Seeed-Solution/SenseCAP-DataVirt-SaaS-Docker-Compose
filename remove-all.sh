7b898a219e22c86ee063d84fa01cae2601923c1c
Mr-feather <2212919407@qq.com>
2020-01-10 09:53:43 +0800

    Update remove-all.sh

--- remove-all.sh
+++ remove-all.sh
@@ -3,14 +3,15 @@
 project_path=$(cd `dirname $0`; pwd)/
 
 cd ${project_path}
-
+echo 'stop and remove  images'
 if [ -d "./SenseCAP-DataVirt-SaaS-Example" ]; then
     cd  SenseCAP-DataVirt-SaaS-Example
-    docker-compose stop -t 1
-    docker-compose kill
-    docker-compose rm -f
+      
 fi
 
-
+docker-compose stop -t 1
+docker-compose kill
+docker-compose rm -f
+echo 'success'
 cd ${project_path}
 rm -rf SenseCAP-DataVirt-SaaS-Example

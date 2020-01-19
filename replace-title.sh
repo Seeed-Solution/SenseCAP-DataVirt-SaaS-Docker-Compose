1e5b004f1b4cf43c1caaa78a065b31e94e5aa95e
Mr-feather <2212919407@qq.com>
2020-01-19 17:04:06 +0800

    fix title space

--- replace-title.sh
+++ replace-title.sh
@@ -5,15 +5,16 @@ cd ${project_path}
 
 
 echo -n "Please Input New Title: "
-read newtitle
+read #newtitle
+echo $REPLY
 
 
-if [ ! $newtitle ]; then
+if [ ! "${REPLY}" ]; then
     echo 'failure'
     exit 1
 fi
 
-docker exec -it webdemo sh -c "grep 'Smart Agriculture Monitoring Data Platform' -rl /usr/share/nginx/html/js | xargs sed -i 's:Smart Agriculture Monitoring Data Platform:"${newtitle}":g'"
-docker exec -it webdemo sh -c "grep '智慧农业监控数据平台' -rl /usr/share/nginx/html/js | xargs sed -i 's:智慧农业监控数据平台:"${newtitle}":g'"
+docker exec -it webdemo sh -c "grep 'Smart Agriculture Monitoring Data Platform' -rl /usr/share/nginx/html/js | xargs sed -i 's:Smart Agriculture Monitoring Data Platform:${REPLY}:g'"
+docker exec -it webdemo sh -c "grep '智慧农业监控数据平台' -rl /usr/share/nginx/html/js | xargs sed -i 's:智慧农业监控数据平台:${REPLY}:g'"
 
-echo "Title changed to: "$newtitle
+echo "Title changed to: "$REPLY
